9566cefb1b4cb39fd840c6f994189c695d37eb5a
Katarzyna Rak <katarzynarak9@gmail.com>
2024-05-24 23:17:18 +0200

    Slider component

---
+++ src/Slider/Slider.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import './Slider.css'
+
+const slides = [
+    { text: 'QUIZ GENERATOR', color: 'rgb(31, 130, 236)' },
+    { text: 'ALL CATEGORIES QUIZ', color: 'red' },
+    { text: 'BOOK QUIZ', color: 'rgb(221, 218, 13)' },
+    { text: 'MUSIC QUIZ', color: 'rgb(10, 137, 141)' },
+    { text: 'FILM QUIZ', color: 'rgb(126, 119, 119)' },
+    { text: 'VIDEO GAMES QUIZ', color: 'rgba(128, 14, 160, 0.925)' },
+    { text: 'SCIENCE QUIZ', color: 'rgb(28, 182, 28)' },
+];
+
+function Slider(){
+    const [currentSlide, setCurrentSlide] = useState(0);
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+      }, 4000);
+      return () => clearInterval(timer);
+    }, []);
+    return(
+        <div className="slider" style={{ backgroundColor: slides[currentSlide].color }}>
+      <h2>{slides[currentSlide].text}</h2>
+    </div>
+    );
+}
+export default Slider
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,9 @@
 import './pages.css';
+import Slider from '../Slider/Slider.jsx'; 
 function MainPage(){
-    
+    return(<div id="container">
+        <Slider></Slider>
+    </div>
+    );
 }
 export default MainPage
