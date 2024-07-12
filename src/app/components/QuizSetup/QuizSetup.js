04f26b163a25f457749cc35c899685dbb9608495
Katarzyna Rak <katarzynarak9@gmail.com>
2024-07-12 11:34:28 +0200

    Quiz rendering fix

--- src/app/components/QuizSetup/QuizSetup.js
+++ src/app/components/QuizSetup/QuizSetup.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import Styles from './QuizSetup.module.css'
 import { Roboto_Mono } from "next/font/google";
-import {Overpass} from "next/font/google";
+import { Overpass } from "next/font/google";
 
 const robotoMono = Roboto_Mono({
     subsets: ['latin'],
@@ -14,8 +14,7 @@ const overpass = Overpass({
     weight: ['800'], 
 });
 
-function QuizSetup(props){
-    let questions = [];  
+function QuizSetup(props) {
     const [bgColor, setBgColor] = useState('white'); 
     const handleMouseEnter = () => setBgColor(props.color); 
     const handleMouseLeave = () => setBgColor('white'); 
@@ -23,61 +22,69 @@ function QuizSetup(props){
     const handleNumberOfQuestions = (event) => setNumberOfQuestions(event.target.value);
 
     const [quizIsCreated, setQuizIsCreated] = useState(false); 
+    const [questions, setQuestions] = useState([]); 
+
     const handleCreateQuiz = async () => {
-        setQuizIsCreated(true);
-        try{
-            const link=`https://opentdb.com/api.php?amount=${numberOfQuestions}${props.category!=="null" ? `&category=${props.category}` : ""}`;
+        try {
+            const link = `https://opentdb.com/api.php?amount=${numberOfQuestions}${props.category !== "null" ? `&category=${props.category}` : ""}`;
             const response = await fetch(link);
             const data = await response.json();
-            console.log(data.results) //gffff
-            for(let i = 0; i < data.results.length; i++){
-                const answers = data.results[i].incorrect_answers;
-                answers.push(data.results[i].correct_answer);
-                let answers2 = []; 
-                for(let j = 0; j < answers.length; j++){
-                    let index = Math.floor(Math.random()*answers.length); 
-                    answers2.push(
-                        <div key={j}>
-                            <input type="radio"/>
-                            <label>{answers[index]}</label>
-                        </div>
-                    )
-                    answers.splice(index,1);
-                }
-                questions.push(
-                    <div key={i} className='question'>
-                        <h3>{data.results[i].question}</h3>
-                        <div className='answers'>{answers2}</div>
+            console.log(data.results);
+
+            const newQuestions = data.results.map((question, index) => {
+                const answers = [...question.incorrect_answers, question.correct_answer];
+                const shuffledAnswers = answers.sort(() => Math.random() - 0.5).map((answer, id) => (
+                    <div key={id}>
+                        <input type="radio" name={`question${index}`} />
+                        <label>{answer}</label>
+                    </div>
+                ));
+                return (
+                    <div key={index} className={Styles.question}>
+                        <h3>{question.question}</h3>
+                        <div className={Styles.answers}>{shuffledAnswers}</div>
                     </div>
                 );
-            } 
-        }catch(error){
+            });
+
+            setQuestions(newQuestions);
+            setQuizIsCreated(true);
+        } catch (error) {
             console.error(error);
         }
-    } 
-    return(
+    };
+
+    return (
         <>
-        {!quizIsCreated && (
-            <div className={`${Styles.container} ${robotoMono.className}`}>
-                <h3>Select the number of questions</h3>
-                <input value={numberOfQuestions} onChange={handleNumberOfQuestions}
-                    className={`${Styles.numberInput} ${robotoMono.className}`}
-                    type="number"
-                    min={1}
-                    max={20}
-                />
-                <button
-                    style={{ backgroundColor: bgColor }}
-                    className={`${Styles.quizbtn} ${overpass.className}`}
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    onClick={handleCreateQuiz}>CREATE QUIZ</button>
-            </div>
-        )}
-        {quizIsCreated && <div className='quiz'>
-            {questions}
-        </div>}
-    </>
+            {!quizIsCreated && (
+                <div className={`${Styles.container} ${robotoMono.className}`}>
+                    <h3>Select the number of questions</h3>
+                    <input 
+                        value={numberOfQuestions} 
+                        onChange={handleNumberOfQuestions}
+                        className={`${Styles.numberInput} ${robotoMono.className}`}
+                        type="number"
+                        min={1}
+                        max={20}
+                    />
+                    <button
+                        style={{ backgroundColor: bgColor }}
+                        className={`${Styles.quizbtn} ${overpass.className}`}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={handleCreateQuiz}
+                    >
+                        CREATE QUIZ
+                    </button>
+                </div>
+            )}
+            {quizIsCreated && (
+                <div className={Styles.quiz}>
+                    {questions}
+                </div>
+            )}
+        </>
     );
 }
+
 export default QuizSetup;
