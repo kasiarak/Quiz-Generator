0710ae3d11793fe53ed5fd8dffec7f01ebea170d
Katarzyna Rak <katarzynarak9@gmail.com>
2024-07-14 20:07:27 +0200

    Science quiz setup

--- src/app/ScienceQuiz/page.jsx
+++ src/app/ScienceQuiz/page.jsx
@@ -1,3 +1,11 @@
+import QuizSetup from "../components/QuizSetup/QuizSetup";
+import '../pages.css';
+
 function ScienceQuizPage(){
+    return(
+        <div className="page">
+            <QuizSetup color="rgb(28, 182, 28)" category="17"></QuizSetup>
+        </div>
+    );
 }
 export default ScienceQuizPage
