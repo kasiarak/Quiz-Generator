a391bf201fd7868e3755fda141ee7177055292ff
Katarzyna Rak <katarzynarak9@gmail.com>
2024-07-14 19:56:35 +0200

    Film quiz setup

--- src/app/FilmQuiz/page.jsx
+++ src/app/FilmQuiz/page.jsx
@@ -1,3 +1,11 @@
+import QuizSetup from "../components/QuizSetup/QuizSetup";
+import '../pages.css';
+
 function FilmQuizPage(){
+    return(
+        <div className="page">
+            <QuizSetup color="rgb(126, 119, 119)" category="11"></QuizSetup>
+        </div>
+    );
 }
 export default FilmQuizPage
