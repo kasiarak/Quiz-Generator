eb8bb4f6e32e3e437787390cfc10068c2f7e18f6
Katarzyna Rak <katarzynarak9@gmail.com>
2024-07-14 19:51:58 +0200

    Music quiz setup

--- src/app/MusicQuiz/page.jsx
+++ src/app/MusicQuiz/page.jsx
@@ -1,3 +1,11 @@
+import QuizSetup from "../components/QuizSetup/QuizSetup";
+import '../pages.css';
+
 function MusicQuizPage(){
+    return(
+        <div className="page">
+            <QuizSetup color="rgb(10, 137, 141)" category="12"></QuizSetup>
+        </div>
+    );
 }
 export default MusicQuizPage
