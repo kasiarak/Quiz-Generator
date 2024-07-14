6de917b4e61f143083c9e8196fc853c6a691f7c2
Katarzyna Rak <katarzynarak9@gmail.com>
2024-07-14 20:02:09 +0200

    Video games quiz setup

--- src/app/VideoGamesQuiz/page.jsx
+++ src/app/VideoGamesQuiz/page.jsx
@@ -1,3 +1,11 @@
+import QuizSetup from "../components/QuizSetup/QuizSetup";
+import '../pages.css';
+
 function VideoGamesQuizPage(){
+    return(
+        <div className="page">
+            <QuizSetup color="rgba(128, 14, 160, 0.925)" category="15"></QuizSetup>
+        </div>
+    );
 }
 export default VideoGamesQuizPage
